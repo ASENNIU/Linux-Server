18063e0c1d0cfeddf437cb6cdf50b7b8cafa5fc9
ASENNIU <1334575226@qq.com>
2023-07-04 16:12:10 +0800

    添加http_conn.h的实现类

--- Chapter_XV_pool/thead_pool/http_conn.h
+++ Chapter_XV_pool/thead_pool/http_conn.h
@@ -98,6 +98,7 @@ private:
     bool add_content(const char* content);
     bool add_status_line(int status, const char* title);
     bool add_headers(int content_length);
+    bool add_content_length(int content_length);
     bool add_linger();
     bool add_blank_line();
 
